f4304ea786d98b9127f8b44d7cfe7722e6c3c803
daisdice <daisdice@gmail.com>
2025-11-28 23:25:18 +0900

    レイアウト調整

--- src/components/Screens/TrophyScreen.tsx
+++ src/components/Screens/TrophyScreen.tsx
@@ -12,7 +12,13 @@ export const TrophyScreen: React.FC = () => {
 
     const formatDate = (timestamp: number) => {
         const date = new Date(timestamp);
-        return date.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        return date.toLocaleString('ja-JP', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
     };
 
     return (
