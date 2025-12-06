fcfe83f181cdc018769225884017db6110b92f63
daisdice <daisdice@gmail.com>
2025-12-06 16:27:49 +0900

    fix: use @fontsource/m-plus-rounded-1c for offline font support

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/m-plus-rounded-1c/400.css';
+import '@fontsource/m-plus-rounded-1c/700.css';
+import '@fontsource/m-plus-rounded-1c/800.css';
 import './styles/global.css'
 import App from './App.tsx'
 
